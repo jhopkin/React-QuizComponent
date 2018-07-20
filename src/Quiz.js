64f7e81993a51a8b8ef66094dbe717a807cffe8a
Jase Hopkin <jhopkin@utah.gov>
2018-07-20 09:36:44 -0600

    Step 4.2

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component{
   }
 
   render(){
-    const isQuizEnd = false//{ this.state.quiz_position - 1 === quizData.quiz_questions.length ? true:false }
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length ? true:false
 
     return (
       <div>
